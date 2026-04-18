28bd5e7d6262d2503d87d6de0af38b154ee7175e
anhminh <minh.nda@teko.vn>
2026-04-18 21:02:07 +0700

    update

--- Program.cs
+++ Program.cs
@@ -52,11 +52,8 @@ var app = builder.Build();
 // --------------------
 // Middleware
 // --------------------
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.MapControllers();
--- obj/Debug/net8.0/Project1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cca4577c2d92bfb3881cfe5c4da0eaf78ff1022")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0401a67829180ae7d7fb531ccab64955556f6f78")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
